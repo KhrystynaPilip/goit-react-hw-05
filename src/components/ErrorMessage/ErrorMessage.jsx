76e14bf63cc5a7402d6659de8821e27d72c46947
Khrystyna Pilip <tina.hristi@gmail.com>
2025-04-10 21:04:34 +0200

    Project settings

--- src/components/ErrorMessage/ErrorMessage.jsx
+++
@@ -1,8 +0,0 @@
-import toast from "react-hot-toast";
-import { useEffect } from "react";
-
-export default function ErrorMessage() {
-  useEffect(() => {
-    toast.error("Something went wrong, try it again...", { duration: 3000 });
-  }, []);
-}
